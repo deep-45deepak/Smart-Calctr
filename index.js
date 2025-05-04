d8f91891f82a510b70e92904d56ba80f3cf838c9
Deepak Kumar <dpk.41deep@gmail.com>
2025-05-04 18:30:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/dijkstras', (req, res) => {
   res.render('Dijikstras');
 });
 
-app.get('/KnapSack', (req, res) => {
+app.get('/knapsack', (req, res) => {
   res.render('KnapSack');
 });
 
